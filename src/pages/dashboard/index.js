e12f79435a91fd3ed70eeed1e64a67f4dd445e35
anlishamaharjan <anlishamaharjan@gmail.com>
2022-04-27 15:12:05 +0545

    Push basic views scaffolding

---
+++ src/pages/dashboard/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Dashboard = props => {
+  return (
+    <View>
+      <Text>Dashboard</Text>
+    </View>
+  );
+};
+
+export default Dashboard;
