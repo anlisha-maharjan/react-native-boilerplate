0431f4102b83b0b91e5e519b46474579438b2ebb
anlishamaharjan <anlishamaharjan@gmail.com>
2022-05-03 11:40:16 +0545

    Fixes status bar color while splashscreen is displayed

--- android/app/src/main/java/com/zpol/MainActivity.java
+++ android/app/src/main/java/com/zpol/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends ReactActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-    SplashScreen.show(this); 
+    SplashScreen.show(this, R.style.SplashTheme, true);
     super.onCreate(savedInstanceState);
   }
   /**
